0e9e23a62adc609bffe85a1f14835be990b25524
shotaro <850123.a@gmail.com>
2025-03-21 12:41:18 +0900

    js-1-5

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const  changeColor = () => {
-  document.body.style.backgroundColor= "gray";
-}
+const  date = () => {
+  const now = new Date();
+  document.getElementById('current-date').textContent = now.toLocaleString();
+};
+date();
