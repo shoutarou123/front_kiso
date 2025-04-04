bf1af249cb1c592a8233655a704a623e00686f65
shotaro <850123.a@gmail.com>
2025-04-04 23:24:01 +0900

    カウントアップ

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+let count = 0;
+
 document.querySelector("button").addEventListener("click", function () {
-  let random = Math.random();
-  document.getElementById("random").textContent = random;
+  count++;
+  document.getElementById("countUp").textContent = count;
 });
