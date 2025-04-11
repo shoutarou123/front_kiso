3752b195ba35b2e9f7d6a273b0c70330205ea245
shotaro <850123.a@gmail.com>
2025-04-11 22:11:45 +0900

    文字数を数える

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-let count = 0;
-
-document.querySelector("button").addEventListener("click", function () {
-  count++;
-  document.getElementById("countUp").textContent = count;
+let input = document.getElementById('input');
+let span = document.getElementById('inputCounter');
+input.addEventListener("keyup", function() {
+  span.textContent = input.value.length;
 });
