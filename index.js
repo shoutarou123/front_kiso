ae162eb4d1efcb6f1929e9d738d08207a82c7897
shotaro <850123.a@gmail.com>
2025-04-18 23:42:15 +0900

    ラジオボタンに色表示

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-let input = document.getElementById('input');
-let span = document.getElementById('inputCounter');
-input.addEventListener("keyup", function() {
-  span.textContent = input.value.length;
-});
+function showSelected() {
+  const radios = document.getElementsByName('color');
+  let selectedColor = '';
+
+  radios.forEach(radio => {
+    if (radio.checked) {
+      selectedColor = radio.value;
+    }
+  });
+
+  const result = document.getElementById('result');
+  if (selectedColor) {
+    result.textContent = `色は「${selectedColor}」です。`;
+  } else {
+    result.textContent = '選択されていません。';
+  }
+}
